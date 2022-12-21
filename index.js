b0c412739413e43a089ec5406ab79f06e0ecbf29
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2022-12-21 15:59:59 -0500

    ANimations

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } 
+  })
+}, {
+  rootMargin: '50% 50% 10% 50%',
+  threshold: 0.2,
+  delay: "100"
+})
 
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
