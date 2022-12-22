13e3901efb63e20773ff92fa2fa1354060e650eb
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2022-12-22 16:18:03 -0500

    Added portfolio page and tweaked mobile

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const observer = new IntersectionObserver((entries) => {
+const headerObserver = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry)
     if (entry.isIntersecting) {
@@ -7,9 +7,34 @@ const observer = new IntersectionObserver((entries) => {
   })
 }, {
   rootMargin: '50% 50% 10% 50%',
-  threshold: 0.2,
+  threshold: 0.01,
   delay: "100"
 })
 
 const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((el) => headerObserver.observe(el));
+
+
+
+const timelineObserver = new IntersectionObserver((entries) => {
+
+  if (window.innerWidth < 900){
+    return
+  }
+
+  entries.forEach((entry) => {
+
+    if (entry.isIntersecting){
+      entry.target.classList.add("card-show");
+    } else {
+      entry.target.classList.remove("card-show");
+    }
+  })
+}, {
+  rootMargin: "50% 0% -130% 0%",
+  threshold: 0.01,
+  delay: "100"
+})
+
+const timelineElements = document.querySelectorAll(".card-hidden");
+timelineElements.forEach(el => timelineObserver.observe(el));
